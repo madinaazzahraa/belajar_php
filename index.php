f08942af119d20f5ef55c0f55bbee18574c38961
madinaazzahraa <madinazahrah23@gmail.com>
2025-01-16 10:58:42 +0700

    add:text

--- index.php
+++ index.php
@@ -6,11 +6,28 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>
-        <?php
-             echo "hello world"
-             ?>
-    </h1>
+  <h1>
+<?php
+
+// konstanta
+define ("gajipokok", 5000000);
+define ("bonus", 500000);
+define ("pajak", 0.12);
+
+// operator penghitungan
+$proyek = 2;
+$total = gajipokok + (bonus * $proyek);
+$gajibersih = $total - pajak;
+
+echo "gaji pokok yang anda peroleh sebesar Rp5.000.000 ";
+echo "<br>";
+echo "total pendapatan anda adalah " . $total ;
+echo "<br>";
+echo "gajibersih seluruh pendapatan anda adalah " . $gajibersih;
+
+
+?>
+  </h1>
     
 </body>
 </html>
